27ae813c6789423e1890cd17d41e990888ee1f2c
Jenny Miggin <jenny@circleci.com>
2022-01-07 16:45:05 +0000

    disable sc2016

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -5,13 +5,13 @@ BuildMessageBody() {
     #   If none, error.
     if [ -n "${SLACK_PARAM_CUSTOM:-}" ]; then
         ModifyCustomTemplate
-        # shellcheck disable=SC2016
+        shellcheck disable=SC2016
         echo "I got here"
         CUSTOM_BODY_MODIFIED=$(echo "$CUSTOM_BODY_MODIFIED" | sed 's/\\\\/\\\\\\\\/g' | sed 's/"/\\\\"/g' | sed 's/`/\\\\`/g')
         T2=$(eval echo \""$CUSTOM_BODY_MODIFIED"\")
     elif [ -n "${SLACK_PARAM_TEMPLATE:-}" ]; then
         TEMPLATE="\$$SLACK_PARAM_TEMPLATE"
-        # shellcheck disable=SC2016
+        shellcheck disable=SC2016
         T1=$(eval echo "$TEMPLATE" | sed 's/\\\\/\\\\\\\\/g' | sed 's/"/\\\\"/g' | sed 's/`/\\\\`/g')
         T2=$(eval echo \""$T1"\")
     else
